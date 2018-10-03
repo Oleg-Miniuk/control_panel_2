3e3c8e8196dc4765e1d95efb3c94b91ab86e3a14
oleg.miniuk@gmail.com <oleg.miniuk@gmail.com>
2018-10-03 15:47:29 +0300

    automatic ui info

--- src/components/AutomaticUI.js
+++ src/components/AutomaticUI.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import Flex from '@core/units/Flex/Flex';
+import styled from 'styled-components';
+import microphone from '../../public_common/microphone2.png';
+
+const Wrapper = styled(Flex).attrs({
+  justify: 'center',
+  mt: '99px',
+  height: '80%'
+})``;
+
+const ImageBackground = styled(Flex)`
+  background: white;
+  width: 50px;
+  border-radius: 100%;
+  height: 49px;
+  position: relative;
+`;
 
 const AutomaticUI = () => (
-  <div className="automatic-ui-wrapper center">
-    wow
-  </div>
+  <Wrapper>
+    <ImageBackground>
+      <img width="50px" alt="microphone icon" src={microphone} />
+    </ImageBackground>
+  </Wrapper>
 );
 
 export default AutomaticUI;
