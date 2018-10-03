01756a8a06291c314f46c5fb6f409b7eebcd082d
oleg.miniuk@gmail.com <oleg.miniuk@gmail.com>
2018-10-03 18:19:13 +0300

    voice record animation

--- src/components/AutomaticUI.js
+++ src/components/AutomaticUI.js
@@ -1,27 +1,18 @@
 import React from 'react';
 import Flex from '@core/units/Flex/Flex';
 import styled from 'styled-components';
-import microphone from '../../public_common/microphone2.png';
+// import microphone from '../../public_common/microphone2.png';
+import Microphone from './Microphone';
 
 const Wrapper = styled(Flex).attrs({
   justify: 'center',
-  mt: '99px',
+  mt: '119px',
   height: '80%'
 })``;
 
-const ImageBackground = styled(Flex)`
-  background: white;
-  width: 50px;
-  border-radius: 100%;
-  height: 49px;
-  position: relative;
-`;
-
 const AutomaticUI = () => (
   <Wrapper>
-    <ImageBackground>
-      <img width="50px" alt="microphone icon" src={microphone} />
-    </ImageBackground>
+    <Microphone />
   </Wrapper>
 );
 
---
+++ src/components/Microphone.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/microphone.css';
+
+const Microphone = () => (
+  <div className="microphone">
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+  </div>
+);
+
+export default Microphone;
